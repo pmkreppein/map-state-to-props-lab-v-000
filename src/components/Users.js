703e16df2c27aafa981cc1fee7da23796b477738
Peter Kreppein <pmkreppein@gmail.com>
2019-01-29 21:04:32 +0000

    Automatically backed up by Learn

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 class Users extends Component {
+    const users = this.props.users.map((user, i) => <li key={i}>{user.userName}</li>)
 
   render() {
     return (
