2fe40f968fcc6768724b1c006d45ebe5bc557c07
Peter Kreppein <pmkreppein@gmail.com>
2019-01-29 21:05:01 +0000

    Automatically backed up by Learn

--- src/components/Users.js
+++ src/components/Users.js
@@ -15,5 +15,12 @@ class Users extends Component {
 }
 
 //add mapStateToProps here
+const mapStateToProps = (state) => {
+  return {
+    users: state.users,
+    primaryUser:state.users[0]
+  }
+}
 
+export const ConnectedUsers = connect(mapStateToProps)(Users)
 export default Users
